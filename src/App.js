dacee8abe13e4d89ddfce042e6a19ae311ea0eb9
Moheed-Dar <moheeddar324@gmail.com>
2024-09-28 17:58:02 +0500

    add some changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import'./App.css'
-import React, {useState} from 'react'
-function App(){
-  const[data,setData]=useState(null)
-  const[print,setPrint]=useState(false)
-  function getdata(val){
-    console.log(val.target.value)
-    setData(val.target.value);
-  }
-  return(
 
+function App(){
+  
+  return(
     <div  className="App  ">
       
-      <input type="text" onChange={getdata} />
-      <button onClick={()=>setPrint(true)}>Print Data</button>
-      {
-        print?
-        <h1>{data}</h1>
-        :null
-      }
     </div>
-
   )
 }
 export default App;
