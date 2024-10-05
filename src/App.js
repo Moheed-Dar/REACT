1ad1693cf909be6d1bbaf50e361c06f4014e41dc
Moheed-Dar <moheeddar324@gmail.com>
2024-10-05 10:03:11 +0500

    add compdidmount

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import'./App.css'
+import React, { useState } from 'react'
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      name:" Mr Saad"
+    }
+  }
+  componentDidMount(){
+    console.log("didmount component")
+  }
+  render(){
+    console.log(this.state.name)
+    return(
+      <div  className="App" >
+        <h2>hello sir { this.state.name }</h2>
+        <button onClick={()=>{this.setState({name:"Mr Saad Khan"})}} >Update  </button>
 
-function App(){
-  
-  return(
-    <div  className="App  ">
-      
-    </div>
-  )
+      </div>
+    );
+  }
 }
 export default App;
 
