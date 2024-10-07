84ba3e976f36bb29394bcb8db2cf1050c5de41b4
Moheed-Dar <moheeddar324@gmail.com>
2024-10-07 11:10:26 +0500

    hooks class 2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import'./App.css'
-import React, { useState } from 'react'
-
+import React, { useState,useEffect } from 'react'
 function App(){
-  const [data,setData] =useState("moheed")
-  console.warn(data.length.valueOf)
+const [data,setData] =useState(0)
+
+  React.useEffect(()=>{
+    console.warn("useeffect")
+  })
+  
  
     return(
       <div  className="App" >
-        <h1>{data}</h1>
-        <button  onClick={()=>setData("moheeddar")} >click</button>
+        <h1>hello world!{data}</h1>
+        <button onClick={()=>setData(data+1)}>click update</button>
+       
       </div>
     );
   }
-
 export default App;
 
 
