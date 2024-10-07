19509e0b3029c1e9acf0dad04fed6453cf966597
Moheed-Dar <moheeddar324@gmail.com>
2024-10-07 09:01:49 +0500

    this is componentdidupdatae with usestate

--- src/App.js
+++ src/App.js
@@ -10,7 +10,10 @@ class App extends React.Component{
     console.log("constructor")
   }
   componentDidUpdate(preProps,preState,snapshot){
-    console.log("didUpdate" , preState);
+    console.log("didUpdate" , preState.count,this.state.count);
+    if(preState.count==this.state.count){
+      alert("you are in same state");
+    }
   }
   
   render(){
@@ -18,7 +21,7 @@ class App extends React.Component{
     return(
       <div  className="App" >
         <h2>hello sir { this.state.count }</h2>
-        <button onClick={()=>{this.setState({count:this.state.count+1})}} >Update  </button>
+        <button onClick={()=>{this.setState({count:+1})}} >Update  </button>
 
       </div>
     );
