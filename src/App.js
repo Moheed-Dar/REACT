20db62a3d2dfad74074317095a08c496d573222e
Moheed-Dar <moheeddar324@gmail.com>
2024-10-07 10:45:54 +0500

    hooks introduction

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import'./App.css'
 import React, { useState } from 'react'
-import Student from './Student'
-class App extends React.Component{
-  constructor(){
-    super();
-    this.state={
-      show:true
-    }
-  
-  }
+
+function App(){
+  const [data,setData] =useState("moheed")
+  console.warn(data.length.valueOf)
  
-  render(){
     return(
       <div  className="App" >
-        {
-          this.state.show?<Student/>:<h2>this component is removed</h2>
-        }
-        <button onClick={()=>this.setState({show:!this.state.show}) }>Update</button>
+        <h1>{data}</h1>
+        <button  onClick={()=>setData("moheeddar")} >click</button>
       </div>
     );
   }
-}
+
 export default App;
 
 
