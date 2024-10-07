40513098e1e43239dd784f4e56e081be36b5fa0e
Moheed-Dar <moheeddar324@gmail.com>
2024-10-07 12:08:15 +0500

    class 3 last part

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import'./App.css'
 import React, { useState,useEffect } from 'react'
+import Student from './Student'
 function App(){
 const [data,setData] =useState(10)
 const [count,setCount] =useState(20)
-  React.useEffect(()=>{
-    console.warn("Data " + data)
-  },[data])
-  React.useEffect(()=>{
-    alert("click start" + count)
-  },[count])
+ 
     return(
       <div  className="App" >
-        <h1>hello world!{data}</h1>
-        <h1>hello world!{count}</h1>
+        <Student data={data} count={count} />
         <button onClick={()=>setData(data+1)}>Data</button>
         <button onClick={()=>setCount(count+1)}>Update</button>
       </div>
