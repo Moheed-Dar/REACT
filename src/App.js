aab5c4491a70a1a9bcb003939caf745901bc82b7
Moheed-Dar <moheeddar324@gmail.com>
2024-10-07 11:25:45 +0500

    hooks class 3 part 1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import'./App.css'
 import React, { useState,useEffect } from 'react'
 function App(){
-const [data,setData] =useState(0)
-
+const [data,setData] =useState(10)
+const [count,setCount] =useState(20)
   React.useEffect(()=>{
-    console.warn("useeffect")
-  })
+    console.warn("Data " + data)
+  },[data])
+  React.useEffect(()=>{
+    alert("click start" +count)
+  },[count])
   
- 
     return(
       <div  className="App" >
         <h1>hello world!{data}</h1>
-        <button onClick={()=>setData(data+1)}>click update</button>
-       
+        <h1>hello world!{count}</h1>
+        <button onClick={()=>setData(data+1)}>Data</button>
+        <button onClick={()=>setCount(count+1)}>Update</button>
       </div>
     );
   }
