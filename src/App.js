676ce00509890982b58712a7720a583baec694d4
Moheed-Dar <moheeddar324@gmail.com>
2024-10-09 21:18:40 +0500

    nested list with bootstrap

--- src/App.js
+++ src/App.js
@@ -2,66 +2,89 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Table } from "react-bootstrap";
 import "./App.css";
 import React from "react";
+
 function App() {
-  // const Students = ["moheed", "zahid", "danish", "usama"];
-  //this is used for array only
-  // in below case we using just map function for data entring inthe table in object types
-  const Students = [
+  const Users = [
     {
-      name: "moheed",
-      work: " bba",
-      Email: "moheeddar@gmail.com",
-      collage: "AUP",
+      name: "moheeddar",
+      class: "it",
+      section: "10",
+      address: [
+        { Hn: "10", city: "muzaffarabad", country: "pakistan" },
+        { Hn: "45", city: "hongkong", country: "china" },
+        { Hn: "32", city: "sharja", country: "dubhi" },
+        { Hn: "87", city: "jadha", country: "saudiarabia" },
+      ],
     },
     {
-      name: "hammad",
-      work: " bba",
-      Email: "hammad@gmail.com",
-      collage: "ABC",
+      name: "zafar",
+      class: "bba",
+      section: "43",
+      address: [
+        { Hn: "10", city: "muzaffarabad", country: "pakistan" },
+        { Hn: "45", city: "hongkong", country: "china" },
+        { Hn: "32", city: "sharja", country: "dubhi" },
+        { Hn: "87", city: "jadha", country: "saudiarabia" },
+      ],
     },
     {
-      name: "hamza",
-      work: " CS",
-      Email: "Ammad@gmail.com",
-      collage: "ABC",
-    },
-    {
-      name: "saad",
-      work: " IT",
-      Email: "saad@gmail.com",
-      collage: "AUP",
-    },
-    {
-      name: "daud",
-      work: " IT",
-      Email: "daud@gmail.com",
-      collage: "AUP",
+      name: "fawad",
+      class: "cs",
+      section:"33",
+      address: [
+        { Hn: "10", city: "muzaffarabad", country: "pakistan" },
+        { Hn: "45", city: "hongkong", country: "china" },
+        { Hn: "32", city: "sharja", country: "dubhi" },
+        { Hn: "87", city: "jadha", country: "saudiarabia" },
+      ],
     },
   ];
+
   return (
     <div>
-      <h2>hello there</h2>
-      <Table striped className="table-warning table-hover ">
-        <tbody>
+      <Table striped variant="dark">
+        <thead>
           <tr>
             <td>name</td>
-            <td>work</td>
-            <td>Email</td>
-            <td>collage</td>
+            <td>class</td>
+            <td>section</td>
+            <td>address</td>
           </tr>
-           {Students.map((data, i) => 
-          data.Email === "hammad@gmail.com"?(
-            <tr key={i}>
-              <td>{data.name}</td>
-              <td>{data.work}</td>
-              <td>{data.Email}</td>
-              <td>{data.collage}</td>
+        </thead>
+        <tbody>
+          {Users.map((item,j) =>(
+            <tr key={j}>
+              <td>{item.name}</td>
+              <td>{item.class}</td>
+              <td>{item.section} </td>
+            <td>
+              <Table striped variant="dark">
+                 <thead>
+                   <tr>
+                      <th>House No</th>
+                      <th>City</th>
+                      <th>Country</th>
+                   </tr>
+                  </thead>
+                <tbody>
+                  {item.address.map((data,i) =>(
+                    <tr key={i}>
+                      <td>{data.Hn} </td>
+                      <td>{data.city} </td>
+                      <td>{data.country} </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            </td>
             </tr>
-          ):null
-          )} 
-        </tbody>
+            ))}
+          </tbody>
       </Table>
     </div>
   );
 }
 export default App;
+
+
+// export default App;
